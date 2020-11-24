7894ad4315a286cd2dbf4cbadbb9ac00a140a536
anesainzt <anesainzt@opendeusto.es>
2020-11-24 13:20:26 +0100

    Ventana creada para cobrar a la gente que haya contratado algun servicio. Todavia está vacia asique se queda en modificable.

---
+++ src/ventanas/VentanaReservaServicio.java
@@ -0,0 +1,16 @@
+package ventanas;
+
+
+import hotel.Cliente;
+
+public class VentanaReservaServicio {
+
+	public VentanaReservaServicio(Cliente cliente, int pago){
+		
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}

d68c6b6e7d34054163ce2a907e3b81bac2c8b7ad
asier <asier@DESKTOP-VCFJJ1E>
2020-11-26 02:08:34 +0100

    Actualizacion de VentanaEmpleado con nombre y apellido del empleado

--- src/ventanas/VentanaEmpleado.java
+++ src/ventanas/VentanaEmpleado.java
@@ -1,11 +1,30 @@
 package ventanas;
 
+import javax.swing.*;
+
 import hotel.*;
 
-public class VentanaEmpleado {
+public class VentanaEmpleado extends JFrame{
 
+	JLabel nombre;
+	JLabel apellido;
+	JPanel pnl;
+	
 	public VentanaEmpleado(Empleado empleado) {
 		
+		nombre = new JLabel(empleado.getNombre());
+		apellido = new JLabel(empleado.getApellido());
+		pnl = new JPanel();
+		
+		
+		
+		pnl.add(nombre);
+		pnl.add(apellido);
+		add(pnl);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setTitle("Ventana para empleados");
+		setSize(800, 200);
+		setVisible(true);
 	}
 	
 }

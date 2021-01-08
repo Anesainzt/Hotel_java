6f78370887411a499f7523b804d658facf52a328
Parra Ledesma <Parra Ledesma@LAPTOP-PD0OAIEP>
2021-01-08 21:23:24 +0100

    Clase con foto

---
+++ src/ventanas/VentanaLogo.java
@@ -0,0 +1,42 @@
+package ventanas;
+
+import java.awt.Graphics;
+import java.awt.Image;
+
+import javax.swing.ImageIcon;
+import javax.swing.JPanel;
+
+public class VentanaLogo extends javax.swing.JFrame{
+	
+	FondoPanel fondo = new FondoPanel();
+	
+	public VentanaLogo() {
+		this.setContentPane(fondo);
+		
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setTitle("Identificación del cliente");
+		setSize(850, 700);
+		setVisible(true);
+	}
+	
+	class FondoPanel extends JPanel{
+		private Image imagen;
+		
+		@Override
+		public void paint(Graphics g) {
+			imagen = new ImageIcon(getClass().getResource("/imagen/hotelJava.jpg")).getImage();
+			
+			g.drawImage(imagen, 0, 0, getWidth(), getHeight(), this);
+			
+			setOpaque(false);
+			
+			super.paint(g);
+		}
+	}
+	
+	
+	public static void main (String args[]) {
+		VentanaLogo vg = new VentanaLogo();
+	}
+}
+

a5fa4e8a796679756e3783835feb4ec2c35f00ac
Parra Ledesma <Parra Ledesma@LAPTOP-PD0OAIEP>
2021-01-17 16:47:26 +0100

    Arerglado problema del minibar

--- src/ventanas/VentanaReservaServicio.java
+++ src/ventanas/VentanaReservaServicio.java
@@ -154,17 +154,20 @@ public class VentanaReservaServicio extends JFrame{
 		    	//SE RESTA 1 AL MES PORQUE EMPIEZA A CONTAR DESDE 0 Y ANTES LE HEMOS SUMADO 1 PARA ESCRIBIR BIEN LA FECHA
 		    	Date minNoche = new Date(Date.UTC(minYear-1900, minMes-1, minDia +1, 0, 0, 0));
 		    	
-		    	if (tipo == "CLASE PADDLE" || tipo == "CLASE NATACION" || tipo == "CLASE FUTBOL-SALA" || tipo == "CLASE BALONCESTO") {
+		    	if (tipo.equals("CLASE PADDLE") || tipo.equals("CLASE NATACION") || tipo.equals("CLASE FUTBOL-SALA") || tipo.equals("CLASE BALONCESTO")) {
 					bd.eleccionClaseDeporte(cliente, fechaRegistro, tipo);
 					VentanaContinuacion vc = new VentanaContinuacion(cliente);
 					dispose();
 		    	
-				} else if (tipo.contains("PADDLE") == true || tipo.contains("NATACION") == true  || tipo.contains("FUTBOL-SALA") == true || tipo.contains("BALONCESTO") == true) {
+				} else if (tipo.equals("PADDLE") || tipo.equals("NATACION") || tipo.equals("FUTBOL-SALA") || tipo.equals("BALONCESTO")) {
 		    		VentanaReservaPista vrp = new VentanaReservaPista(cliente, fechaRegistro, tipo, precio);
 		    		dispose();
-				}else if(tipo.contains("MASAJE FACIAL") == true || tipo.contains("MASAJE CORPORAL") == true || tipo.contains("MASAJE TOTAL") == true || tipo.contains("JACUZZI") == true || tipo.contains("SALES MINERALES") == true || tipo.contains("MASAJE PIEDRAS") == true || tipo.contains("TRATAMIENTO ESTÉTICO") == true) {
+				}else if(tipo.equals("MASAJE FACIAL") || tipo.equals("MASAJE CORPORAL") || tipo.equals("MASAJE TOTAL") || tipo.equals("JACUZZI") || tipo.equals("SALES MINERALES") || tipo.equals("MASAJE PIEDRAS") || tipo.equals("TRATAMIENTO ESTÉTICO")) {
 					VentanaReservaSpa vrs = new VentanaReservaSpa(cliente, fechaRegistro, tipo, precio);
 		    		dispose();
+				}else if(tipo.equals("VODKA") || tipo.equals("GINEBRA") || tipo.equals("WHISKEY") || tipo.equals("TEQUILA") || tipo.equals("RON") || tipo.equals("ELECCION DE DOS") || tipo.equals("ELECCION DE TRES") || tipo.equals("TODOS")) {
+					VentanaContinuacion vc = new VentanaContinuacion(cliente);
+					dispose();
 				}else{
 					VentanaReservaReunion vrr = new VentanaReservaReunion(cliente, fechaRegistro, tipo, precio);
 					dispose();
